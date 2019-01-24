ab368b5aef20ff94d278e4fdd9949fc3a02965cc
Arul Kandasamy <akandasamy@virtusa.com>
2019-01-24 21:14:27 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "gcs" {
+    bucket = "arul_terraform_states"
+    prefix = "gitlab/gitlab.tfstate"
+    credentials = "terraformpoc-229221-b7f45204dce7.json"
+  }
+}
+
+module "gitlabci" {
+  source = "./gitlab"
+  project = "${var.project}"
+  external_ip = "${module.network.address}"
+  deploy_gitlab = "${var.deploy_gitlab}"
+}
+
+module "network" {
+  source = "./network"
+  project = "${var.project}"
+  source_ranges = "${var.source_ranges}"
+  external_address_name = "gitlab-ce-enternal-ip"
+  generate_external_ip = "${var.generate_external_ip}"
+}
+
+module "gitlab-runner" {
+  source = "./gitlab-runner"
+  runner_count = "${var.runner_count}"
+  project = "${var.project}"
+  runner_host = "${data.template_file.runner_host.rendered}"
+  runner_token = "${module.gitlabci.runner_token}"
+}
